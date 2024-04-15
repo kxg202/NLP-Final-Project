0d24033cbdf030358fece484d4a5b4454a11f4f8
kxg202 <kevinxgong@gmail.com>
2024-04-14 20:25:11 -0400

    Added starter code

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,22 @@
 ## Methods to preprocess the data go here.
+from sklearn.model_selection import train_test_split
+
+def preprocessCSV(filepath):
+    f = open("datasets" + filepath)
+    return x_train, x_test, y_train, y_test
+
+def preprocessJSON(filepath):
+    f = open("datasets" + filepath)
+    return x_train, x_test, y_train, y_test
+
+def preprocess(filepath):
+    file_extension = filepath.split(".")[-1]
+    extensionMap = {
+        "json": preprocessJSON,
+        "csv": preprocessCSV
+    }
+    if file_extension in extensionMap:
+        return extensionMap[file_extension](filepath)
+    else:
+        print("Unsupported file extension:", file_extension)
+    
--- system.py
+++ system.py
@@ -3,5 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
-tfidf_vectorizer = TfidfVectorizer()
-classifier = LogisticRegression()
+def train_and_predict(X_train, X_test, Y_train, Y_test):
+    X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+    X_test_tfidf = tfidf_vectorizer.transform(X_test)
+    tfidf_vectorizer = TfidfVectorizer(ngram_range(1,2))
+    classifier = LogisticRegression()
+    classifier.fit(X_train_tfidf, y_train)
+    y_pred = classifier.predict(X_test_tfidf)
+    return y_pred
